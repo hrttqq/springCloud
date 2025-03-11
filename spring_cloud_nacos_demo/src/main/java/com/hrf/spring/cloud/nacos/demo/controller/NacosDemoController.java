a25fa46300bf3e4079a597de9e85984e18976ee1
“hrttqq” <hrttqq@sina.cn>
2025-03-11 15:10:41 +0800

    gateway服务demo搭建

---
+++ spring_cloud_nacos_demo/src/main/java/com/hrf/spring/cloud/nacos/demo/controller/NacosDemoController.java
@@ -0,0 +1,19 @@
+package com.hrf.spring.cloud.nacos.demo.controller;
+
+import com.hrf.spring.cloud.nacos.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/nacosDemo")
+public class NacosDemoController {
+
+    @Autowired
+    private DemoService demoService;
+
+    @RequestMapping("/test")
+    public String test() {
+        return demoService.testDemo();
+    }
+}
